e29427162329c3b10a1e67cec51de920b820e507
BFlorian91 <florianbeaumont412@gmail.com>
2019-10-17 23:51:49 +0200

    fix 06

--- Day03/ex06/members_only.php
+++ Day03/ex06/members_only.php
@@ -1,19 +1,9 @@
 <?php
-    $user = 'zaz';
-    $pass = 'jaimelespetitsponeys';
-
-    if (!isset($_SERVER['PHP_AUTH_USER']) || !isset($_SERVER['PHP_AUTH_PW'])) {
-        header('WWW-Authenticate: Basic realm="My Realm"');
+    if (($_SERVER['PHP_AUTH_USER'] != "zaz") || ($_SERVER['PHP_AUTH_PW'] != "jaimelespetitsponeys")){
         header('HTTP/1.0 401 Unauthorized');
-        die;
-    }
-    $data = file_get_contents('../img/42.png');
-    $img = 'data:image/png;base64,' . base64_encode($data);
-    if ($_SERVER['PHP_AUTH_USER'] == $user && $_SERVER['PHP_AUTH_PW'] == $pass) { ?>
-    <html><body>
-        Bonjour, Zaz<br/>
-        <img src="<?php echo $img ?>">
-    </body></html>
-<?php } else { ?>
-        <html><body>Cette zone est accessible uniquement aux membres du site</body></html>
-<?php } ?>
+        header("WWW-Authenticate: Basic realm='My Realm'");
+        echo "<html><body>Cette zone est accessible uniquement aux membres du site</body></html>" . PHP_EOL;
+    } else {
+        $file = file_get_contents('../img/42.png');
+        echo "<html><body>" . PHP_EOL . "Bonjour Zaz<br />" . PHP_EOL . "<img src='data:image/jpeg;base64," . base64_encode($file) . "'>" . PHP_EOL . "</body></html>" . PHP_EOL;
+	}
