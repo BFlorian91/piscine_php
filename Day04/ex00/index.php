67bf0ef01af444a88f19def286aa13138890a820
BFlorian91 <florianbeaumont412@gmail.com>
2019-10-19 00:09:55 +0200

    fix error ex00

--- Day04/ex00/index.php
+++ Day04/ex00/index.php
@@ -1,19 +1,17 @@
 <?php
     session_start();
-    if (isset($_GET['login']) && isset($_GET['passwd']) && isset($_GET['submit'])) {
+    if ($_GET['login'] && $_GET['passwd'] && $_GET['submit']) {
         if ($_GET['submit'] == 'OK') {
             $_SESSION['login'] = $_GET['login'];
             $_SESSION['passwd'] = $_GET['passwd'];
         }
     }
 ?>
-
-<!DOCTYPE html>
 <html><body>
-    <form action="index.php" method="get">
-        <?php echo 'Identifiant: '?><input type="text" name="login" value="<?php echo $_SESSION['login']?>">
-        <br />
-        <?php echo 'Mot de passe: '?><input type="password" name="passwd" value="<?php echo $_SESSION['passwd']?>">
-        <button type="submit" name="submit" value="OK"></button>
-    </form>    
+<form action="index.php" method="get">
+    <?php echo 'Identifiant: '?><input type="text" name="login" value="<?php echo $_SESSION['login']?>" />
+    <br />
+    <?php echo 'Mot de passe: '?><input type="password" name="passwd" value="<?php echo $_SESSION['passwd']?>" />
+    <input type="submit" name="submit" value="OK" />
+</form>    
 </body></html>
