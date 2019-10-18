e46fb25f3a8ec8b405c891b76910681b69bf4502
Florian <flbeaumo@student.42.fr>
2019-10-18 18:19:59 +0200

    starting chat

---
+++ Day04/ex04/whoami.php
@@ -0,0 +1,8 @@
+<?php
+    session_start();
+    
+    if ($_SESSION["loggued_on_user"]) {
+        echo $_SESSION["loggued_on_user"] . PHP_EOL;
+    } else {
+        echo "ERROR" . PHP_EOL;
+    }
