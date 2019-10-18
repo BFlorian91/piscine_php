79fbdd11f3b090c254a8b23fa96a86fa783328ca
Florian <flbeaumo@student.42.fr>
2019-10-18 17:49:24 +0200

    all is ok

--- Day03/ex06/members_only.php
+++ Day03/ex06/members_only.php
@@ -5,5 +5,5 @@
         echo "<html><body>Cette zone est accessible uniquement aux membres du site</body></html>" . PHP_EOL;
     } else {
         $file = file_get_contents('../img/42.png');
-        echo "<html><body>" . PHP_EOL . "Bonjour Zaz<br />" . PHP_EOL . "<img src='data:image/jpeg;base64," . base64_encode($file) . "'>" . PHP_EOL . "</body></html>" . PHP_EOL;
+        echo "<html><body>" . PHP_EOL . "Bonjour Zaz<br />" . PHP_EOL . "<img src='data:image/png;base64," . base64_encode($file) . "'>" . PHP_EOL . "</body></html>" . PHP_EOL;
 	}
