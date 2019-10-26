bba91edcb7b2c9595cbd1848bdaa861c58e15c69
BFlorian91 <florianbeaumont412@gmail.com>
2019-10-26 15:45:00 +0200

    fuck this piscine !

--- Day09/ex02/todo.js
+++ Day09/ex02/todo.js
@@ -1,51 +1,53 @@
-let button = document.getElementById("newTask");
-let lst = document.getElementById("ft_list");
+var button = document.getElementById('new');
+var lst = document.getElementById('ft_list');
 loadCookie();
 
-button.onclick = () => { addTodo(prompt("Enter a new thing to do"))};
+button.onclick = function () {addTodo(prompt('Enter new thing to do'))};
 
-function createCookie() {
-    let el = lst.firstChild;
-    let str = '';
-    while ((el !== null)) {
-        if (typeof el.innerHTML !== 'undefined')
-            str = el.innerHTML + '\\' + str;
-        el = el.nextSibling;
-    }
-    document.cookie = 'todo=' + str + '; expires=Fri, 31 Dec 9999 23:59:59 GMT path=/;';
+function removeDiv(elem) {
+  if (confirm('Do you really want to delete this element?')) {
+    elem.remove();
+    createCookie();
+  }
 }
 
-function removeDiv(el) {
-    if (confirm("Do you really want to delete this element?")) {
-        el.remove();
-        createCookie();
-    }
+function createCookie() {
+  var elem = lst.firstChild;
+  var str = '';
+  while((elem) !== null) {
+    if (typeof elem.innerHTML !== 'undefined')
+      str = elem.innerHTML + '\\' + str;
+    elem = elem.nextSibling
+  }
+  document.cookie = 'todo=' + str + '; expires=Fri, 31 Dec 9999 23:59:59 GMT path=/;';
 }
 
 function addTodo(todo) {
-    if (todo === null || todo == '')
-        return false;
-    let div = document.createElement('div');
-    let id = parseInt(lst.firstChild.id);
-    console.log(id);
-    div.innerHTML = todo;
-    div.onclick = () => { removeDiv(div); };
-    if (!isNaN(id))
-        div.id = id + 1;
-    else
-        div.id = 1;
-    lst.insertBefore(div, lst.firstChild);
-    createCookie(todo);
+  if (todo === null || todo == '')
+    return false;
+  var div = document.createElement('div');
+  var id = parseInt(lst.firstChild.id);
+  div.innerHTML = todo;
+  div.onclick = function() {
+    removeDiv(div);
+  };
+  if (!isNaN(id)) {
+    div.id = id + 1;
+  } else {
+    div.id = 1;
+  }
+  lst.insertBefore(div, lst.firstChild);
+  createCookie(todo);
 }
 
 function loadCookie() {
-    if (document.cookie.includes('todo=')) {
-        let str = document.cookie.split(';');
-        let cookie = str[0].split('=');
-        if (cookie[0] == 'todo') {
-            let todo = cookie[1].split('\\');
-            for (i = 0; todo[i]; i++)
-                addTodo(todo[i]);
-        }
+  if (document.cookie.includes('todo=')) {
+    var str = document.cookie.split(';');
+    var cookie = str[0].split('=');
+    if (cookie[0] == 'todo') {
+      var todos = cookie[1].split('\\');
+      for (i = 0; todos[i]; i++)
+      addTodo(todos[i]);
     }
+  }
 }
